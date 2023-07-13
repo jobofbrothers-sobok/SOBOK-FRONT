ee02c742a8a9b9ba3072443e36ef72bfb0c9e196
이효원 <ymj07168@gmail.com>
2023-07-13 14:16:42 +0900

    fix: 배포시 proxy 서버 통신 오류 해결

---
+++ src/lib/api/main.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const PROXY = window.location.hostname === 'localhost' ? '/api' : '/proxy/api';
+
+// 카페 찜하기
+export const postLike = async (storeId, config) => {
+    return await axios.post(`${PROXY}/main/store/${storeId}}`, {}, config);
+};
+
+// 카페 찜 해제하기
+export const deleteLike = async (storeId, config) => {
+    return await axios.delete(`${PROXY}/main/store/${storeId}`, config);
+};
+
+// 카페 상세 정보 조회
+export const getCafeInfo = async (storeId, config) => {
+    return await axios.get(`${PROXY}/main/store/info/${storeId}`, {}, config);
+};
+
+// 카페 상세 소식 조회
+export const getCafeNews = async (storeId, query, config) => {
+    return await axios.get(`${PROXY}/main/store/notice/${storeId}?query=${query}`, {}, config);
+};
+
+// 카페 상세 메뉴 조회
+export const getCafeMenu = async (storeId, config) => {
+    return await axios.get(`${PROXY}/main/store/menu/${storeId}`, {}, config);
+};
+
+// 카페 상세 리뷰 조회
+export const getCafeReviews = async (storeId, config) => {
+    return await axios.get(`${PROXY}/main/store/review/${storeId}`, {}, config);
+};
+
+// 스토어 전체 상품 조회
+export const getProducts = async (storeId, config) => {
+    return await axios.get(`${PROXY}/main/store/products/${storeId}`, {}, config);
+};
+
+
+
--- src/lib/api/stamp.js
+++ src/lib/api/stamp.js
@@ -8,7 +8,7 @@ const config = {
     }
 }
 
-const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+const PROXY = window.location.hostname === 'localhost' ? '/api' : '/proxy/api';
 
 // 고객 스탬프 적립 코드 생성
 export const getCode = async (config) => {
