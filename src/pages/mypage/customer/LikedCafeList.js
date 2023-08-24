2054f7c5d44d8e894cfcb08ffedf0c02fc77fbfc
이효원 <ymj07168@gmail.com>
2023-08-25 00:38:40 +0900

    fix: 찜한 카페 전체보기 이미지 주소 오류 수정

--- src/pages/mypage/customer/LikedCafeList.js
+++ src/pages/mypage/customer/LikedCafeList.js
@@ -52,7 +52,7 @@ const LikedCafeList = () => {
                             <CafeItem
                                 id={item.id}
                                 key={item.id}
-                                image={`https:/b.sobok.co.kr/${item.image}`}
+                                image={`https://b.sobok.co.kr/${item.image}`}
                                 title={item.storeName}
                                 distance={item.distance >= 1000 ? Math.round(item.distance / 1000) + 'km' : Math.round(item.distance) + 'm'}
                                 intro={item.description}
